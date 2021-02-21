6a201ec7d44f226bacdb796982ea4528679c5e30
sdk40010 <sdk40010@gmail.com>
2021-02-21 17:50:42 +0900

    ルーティングファイルの修正

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['camelToSnake', 'snakeToCamel'])->prefix('v1')->group(function () {
+    // 認証が不要なルート
     Route::post('/login', 'Auth\LoginController@login');
     Route::post('/logout', 'Auth\LoginController@logout');
     Route::get('/login/check', 'Auth\LoginController@check');
 
-    Route::middleware('auth')
-        ->prefix('invitations')->group(function () {
-            Route::get('/', 'InvitationController@index');
+    Route::prefix('invitations')->group(function () {
+        Route::get('/', 'InvitationController@index');
+    });
+
+    // 認証が必要なルート
+    Route::middleware('auth')->prefix('invitations')
+        ->group(function () {
             Route::get('/{invitation}', 'InvitationController@show');
             Route::post('/', 'InvitationController@store');
             Route::put('/{invitation}', 'InvitationController@update');
